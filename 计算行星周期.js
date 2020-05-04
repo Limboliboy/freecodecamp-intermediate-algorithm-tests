b30c3ec9aab90480095e6e6c60adf403a1f5067e
Limboliboy <1265780779@qq.com>
2020-05-04 09:04:07 +0800

    change

--- 计算行星周期.js
+++ 计算行星周期.js
@@ -1,3 +1,13 @@
+/*写一个计算天体轨道周期的函数，接收一个对象数组参数arr，包含表示天体名称的name属性，及表示轨道半长轴的avgAlt属性，就像这样：
+{name: 'name', avgAlt: avgAlt}
+返回值也是一个对象数组，留原对象中的name属性和值，根据avgAlt属性的值求出
+轨道周期（单位是秒），并赋值给orbitalPeriod属性。返回值中不应保留原数据中的avgAlt属性及其对应的值。
+示例：
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}])
+返回 [{name: "sputnik", orbitalPeriod: 86400}]
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) 
+返回 [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}]
+*/
 /*function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
